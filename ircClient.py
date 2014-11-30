7bf880278de02fb47208343b6bb6290487627520
mmitaru <mateim@pdx.edu>
2014-11-30 12:54:52 -0800

    completed to the point of turning in

--- ircClient.py
+++ ircClient.py
@@ -1,9 +1,21 @@
 #! /usr/bin/env python
 
-# FROM: http://code.activestate.com/recipes/531824-chat-server-client-using-selectselect/
-# FROM: http://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
-# Matei Mitaru Networks Project CS494 Fall 2014 Portland State University
-# Simple TCP socket server/client IRC protocol implementation using the python select module
+
+# Matei Mitaru, Networks Project, CS494, Fall 2014, Portland State University.
+# Simple TCP socket server/client IRC protocol implementation using the python select module.
+# 3 data structures are used to track client info: clients-nickname map, rooms-[clients] map, and clients-[rooms] map.
+# 2 dispatch dictionaries mapping command literals to class methods.
+# Using the select module, the control loop examines inputs stdin and socket connections to the server.
+# Input data is examined and processed accordingly, and client messages are delivered when needed.
+
+# SOURCES:
+# http://code.activestate.com/recipes/578591-primitive-peer-to-peer-chat/
+# http://code.activestate.com/recipes/531824-chat-server-client-using-selectselect/
+# http://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
+# https://docs.python.org/2/library/select.html
+# https://docs.python.org/2/library/socket.html
+# https://docs.python.org/2/library/signal.html
+
 
 import socket
 import sys
@@ -13,51 +25,48 @@ import time
 BUFSIZ = 1024
 CHILL = .01
 
-class ChatClient():
-    """ A simple command line chat client using select """
+class ChatClient(object):
 
     def __init__(self):
         self.name = 'GUEST'
-        # Quit flag
+        # quit flag
         self.flag = False
         self.port = 6667
         self.host = 'localhost'
         self.rooms = []
         self.rooms.append('Lobby')
-        # Initial prompt
+        # prompt
         self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
-        # Command list
+        # command list (1 arg)- dictionary mapping command literals to class methods
         self.dispatch1 = {
             'NICK': self.do_nick,
             'CREATE': self.do_create,
             'JOIN': self.do_join,
             'PART': self.do_leave,
-            'QUIT': self.do_quit,
-            'PING': self.do_ping,
-            'PONG': self.do_pong
+            'QUIT': self.do_quit
         }
+        # command list (0 arg)- dictionary mapping command literals to class methods
         self.dispatch0 = {
             'LISTROOMS': self.do_listrooms,
             'LISTALLROOMS': self.do_listallrooms,
             'QUIT': self.do_quit,
             'NAMES':self.do_names
         }
-        # Connect to server at port
+        # register first time user
+        print "\nPlease enter your nickname >"
+        self.name = sys.stdin.readline().strip()
+        # connect to server at port
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((self.host, self.port))
-            print 'Connected to chat server@%d' % self.port
-            # Send my name...
-            # send(self.sock,'NAME: ' + self.name)
+            print "Connected to chat server"
             self.sock.send('NAME: ' + self.name)
-            data = self.sock.recv(BUFSIZ)
-            # Contains client address, set it
-            #addr = data.split('CLIENT: ')[1]
             self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
         except socket.error, e:
-            print 'Could not connect to chat server @%d' % self.port
+            print "Could not connect to chat server"
             sys.exit(1)
 
+    # update nickname
     def do_nick(self, arg):
         self.name = arg
         self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
@@ -71,6 +80,7 @@ class ChatClient():
             self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
         else: print self.prompt + "client error"
 
+    # join room
     def do_join(self, arg):
         # need to handle received "error"!
         temp = self.sock.recv(BUFSIZ).split()
@@ -79,6 +89,7 @@ class ChatClient():
             self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
         else: print self.prompt + "client error"
 
+    # leave a room ('PART')
     def do_leave(self, arg):
         temp = self.sock.recv(BUFSIZ).split()
         if (arg in self.rooms) and temp[0] != "error":
@@ -86,72 +97,73 @@ class ChatClient():
             self.prompt='[' + '@'.join((self.name, '#' + '#'.join(self.rooms))) + ']> '
         else: print self.prompt + "client error"
 
+    # leave chat
     def do_quit(self):
         time.sleep(CHILL)
         print "\nclosing connection..."
         self.flag = True
         self.sock.close()
 
+    # list rooms that this client belongs to
     def do_listrooms(self):
         time.sleep(CHILL)
 
+    # list all available rooms
     def do_listallrooms(self):
         time.sleep(CHILL)
 
+    # list names of all users that share at least one room with this client
     def do_names(self):
         time.sleep(CHILL)
 
-    def do_ping(self, arg):
-        print "PING command received"
-
-    def do_pong(self, arg):pass
-
+    # execute command (1 arg)
     def processcommand(self, command, arg):
         command = self.dispatch1[command]
         command(arg)
 
+    # execute command (no args)
     def processnoargcommand(self, command):
         command = self.dispatch0[command]
         command()
 
     def control(self):
-
+        # control loop
         while not self.flag:
             try:
+                # display prompt
                 sys.stdout.write(self.prompt)
                 sys.stdout.flush()
-
                 # check for input from stdin and socket
                 inputready, outputready,exceptready = select.select([0, self.sock], [],[])
-
                 for i in inputready:
                     if i == 0:
-                        # send data from stdin to socket (server)
+                        # read input from stdin
                         data = sys.stdin.readline().strip()
                         temp = data.split()
                         if data:
+                            # send input data to the server
                             self.sock.send(data)
+                            # if client user issued a command, process it
                             if temp[0] in self.dispatch1.keys() and len(temp) > 1:
                                 self.processcommand(temp[0], temp[1])
                             elif temp[0] in self.dispatch0.keys():
                                 self.processnoargcommand(temp[0])
                     elif i == self.sock:
+                        # read input from socket
                         data = self.sock.recv(BUFSIZ)
                         # handle broken connection
                         if not data:
-                            print 'Shutting down.'
+                            print 'Shutting down...'
                             self.flag = True
                             break
                         else:
                             # print received messages
                             sys.stdout.write(data + '\n')
                             sys.stdout.flush()
-
             except KeyboardInterrupt:
-                print "closing connection..."
+                print "Closing connection..."
                 self.sock.close()
                 break
-
 
 if __name__ == "__main__":
     client = ChatClient()
--- ircServer.py
+++ ircServer.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
-# FROM: http://code.activestate.com/recipes/531824-chat-server-client-using-selectselect/
-# FROM: http://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
-# Matei Mitaru Networks Project CS494 Fall 2014 Portland State University
-# Simple TCP socket server/client IRC protocol implementation using the python select module
+
+# Matei Mitaru, Networks Project, CS494, Fall 2014, Portland State University.
+# Simple TCP socket server/client IRC protocol implementation using the python select module.
+# 3 data structures are used to track client info: clients-nickname map, rooms-[clients] map, and clients-[rooms] map.
+# 2 dispatch dictionaries mapping command literals to class methods.
+# Using the select module, the control loop examines inputs stdin and socket connections to the server.
+# Input data is examined and processed accordingly, and client messages are delivered when needed.
+
+# SOURCES:
+# http://code.activestate.com/recipes/578591-primitive-peer-to-peer-chat/
+# http://code.activestate.com/recipes/531824-chat-server-client-using-selectselect/
+# http://stackoverflow.com/questions/960733/python-creating-a-dictionary-of-lists
+# https://docs.python.org/2/library/select.html
+# https://docs.python.org/2/library/socket.html
+# https://docs.python.org/2/library/signal.html
+
 
 import select
 import socket
@@ -13,27 +25,26 @@ from collections import defaultdict
 
 BUFSIZ = 1024
 
-class ChatServer():
+class ChatServer(object):
 
     def __init__(self, port=6667, backlog=5):
+        # keeps count of connected clients
         self.clients = 0
-        # Client map
+        # maps clients to nicknames
         self.clientmap = {}
-        # Room map
+        # maps rooms to [clients]
         self.roommap = defaultdict(list)
-        # client-room-map
+        # maps clients to [rooms]
         self.clientroommap = defaultdict(list)
-        # Command list: 1 arg commands
+        # command list: 1 arg commands - maps command literals to class methods
         self.dispatch1 = {
             'NICK': self.do_nick,
             'CREATE': self.do_create,
             'JOIN': self.do_join,
             'PART': self.do_leave,
-            'PING': self.do_ping,
-            'PONG': self.do_pong,
             '#': self.do_broadcasttoroom
         }
-        # Command list: 0 arg commands
+        # command list: 0 arg commands - maps command literals to class methods
         self.dispatch0 = {
             'LISTROOMS': self.do_listrooms,
             'LISTALLROOMS': self.do_listallrooms,
@@ -44,51 +55,48 @@ class ChatServer():
         self.outputs = []
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind(('',port))
-        print 'Listening to port',port,'...'
+        self.server.bind(('', port))
+        print "Listening on port", port, "..."
         self.server.listen(backlog)
-        # Trap keyboard interrupts
-        signal.signal(signal.SIGINT, self.sighandler)
+        # catch interrupts, pass in our own handler
+        signal.signal(signal.SIGINT, self.signalhandler)
 
-    def sighandler(self, signum, frame):
-        # Close the server
-        print 'Shutting down server...'
-        # Close existing client sockets
+    # 'signum' and 'frame' are required params for the signal handler passed to signal()
+    def signalhandler(self, signum, frame):
+        print "Shutting down server..."
+        # close existing client sockets
         for o in self.outputs:
             o.close()
-
         self.server.close()
 
+    # return the nickname of a client connection
     def getname(self, client):
         info = self.clientmap[client]
         return info[1] + '@'
 
-
+    # accept a connection from a client
     def connectclient(self, inputs):
         # handle the server socket
         client, address = self.server.accept()
-        print 'chatserver: got connection %d from %s' % (client.fileno(), address)
-        # Read the login name
+        print "New client connected..."
+        # register new user, place in 'Lobby'
         cname = client.recv(BUFSIZ).split('NAME: ')[1]
-
-        # Compute client name and send back
         self.clients += 1
-        client.send('CLIENT: ' + str(address[0]))
         inputs.append(client)
         self.roommap['Lobby'].append(client)
         self.clientroommap[client].append('Lobby')
-
         self.clientmap[client] = (address, cname)
-        # Send joining information to other clients
-        msg = '\n(Connected: New client (%d) from %s)' % (self.clients, self.getname(client))
+        # let other clients know there is a new user
+        msg = "Connected: New client- " + self.getname(client)
         for o in self.outputs:
             o.send(msg)
-
         self.outputs.append(client)
 
+    # update nickname
     def do_nick(self, client, arg):
         self.clientmap[client] = (self.clientmap[client][0], arg)
 
+    # create room
     def do_create(self, client, arg):
         if not self.roommap.has_key(arg):
             self.roommap[arg].append(client)
@@ -97,6 +105,7 @@ class ChatServer():
         else:
             client.send("error")
 
+    # join room
     def do_join(self, client, arg):
         if self.roommap.has_key(arg):
             if not arg in self.clientroommap[client]:
@@ -108,6 +117,7 @@ class ChatServer():
         else:
             client.send("error")
 
+    # leave room ('PART')
     def do_leave(self, client, arg):
         if self.roommap.has_key(arg) and arg in self.clientroommap[client]:
             self.roommap[arg].remove(client)
@@ -116,31 +126,32 @@ class ChatServer():
         else:
             client.send("error")
 
+    # quit chat
     def do_quit(self, client):
         for r in self.roommap:
             if client in self.roommap[r]:
                 self.roommap[r].remove(client)
         self.clientroommap.pop(client)
+        self.clients -= 1
 
-
+    # list rooms client belongs to
     def do_listrooms(self, client):
         for r in self.clientroommap[client]:
             client.send("\n" + r)
 
+    # list all rooms
     def do_listallrooms(self, client):
         for r in self.roommap:
             client.send("\n" + r)
 
+    # list names of users who share rooms with client
     def do_names(self, client):
         for r in self.clientroommap[client]:
             for c in self.roommap[r]:
                 if self.getname(c) != self.getname(client):
                     client.send("\n" + self.getname(c) + r)
 
-    def do_ping(self, client, arg):pass
-
-    def do_pong(self, client, arg):pass
-
+    # broadcast message to specified room ('#roomname')
     def do_broadcasttoroom(self, client, arg):
         data = arg.split()
         room = data[0].strip('#')
@@ -154,28 +165,27 @@ class ChatServer():
                     msg = '\n#[' + self.getname(client) + room + ']>> ' + message
                     c.send(msg)
 
-
-
+    # execute command with arg
     def processcommand(self, client, command, arg):
         command = self.dispatch1[command]
         command(client, arg)
 
+    # execute command without arg
     def processnoargcommand(self, client, command):
         command = self.dispatch0[command]
         command(client)
 
+    # process incoming data, send out appropriate responses
     def handleclients(self, inputs, s):
-        # handle all other sockets
         try:
+            # examine the data
             data = s.recv(BUFSIZ)
-            # data = receive(s)
             if data:
-
                 temp = data.split()
                 # commands that take an argument
                 if temp[0] in self.dispatch1.keys():
                     if len(temp) == 1:
-                        print temp[0] + " requires at least one argument"
+                        print temp[0] + " requires at least one argument."
                     else:
                         self.processcommand(s, temp[0], temp[1])
                 # broadcast to specific room only
@@ -189,12 +199,6 @@ class ChatServer():
                 elif temp[0] in self.dispatch0.keys():
                     self.processnoargcommand(s, temp[0])
                 else:
-                    # Send as new client's message...
-                    #msg = '\n#[' + self.getname(s) + ']>> ' + data
-                    # Send data to all except ourselves
-                    #for o in self.outputs:
-                    #    if o != s:
-                    #        o.send(msg)
                     # send data to clients in our rooms
                     for r in self.clientroommap[s]:
                         for o in self.roommap[r]:
@@ -202,44 +206,36 @@ class ChatServer():
                                 msg = '\n#[' + self.getname(s) + r + ']>> ' + data
                                 o.send(msg)
             else:
-                print 'chatserver: %d hung up' % s.fileno()
+                print "Client hung up"
                 self.clients -= 1
                 s.close()
                 inputs.remove(s)
                 self.outputs.remove(s)
-
-                # Send client leaving information to others
-                msg = '\n(Hung up: Client from %s)' % self.getname(s)
+                # send client leaving information to others
+                msg = self.getname(s) + " has left chat."
                 for o in self.outputs:
                     o.send(msg)
 
         except socket.error, e:
-            # Remove
+            # remove lost client
             inputs.remove(s)
             self.outputs.remove(s)
 
     def serve(self):
-
         inputs = [self.server,sys.stdin]
         self.outputs = []
-
         running = 1
-
         while running:
-
             try:
                 inputready,outputready,exceptready = select.select(inputs, self.outputs, [])
             except select.error, e:
                 break
             except socket.error, e:
                 break
-
             for s in inputready:
-
                 if s == self.server:
                     # handle the server socket
                     self.connectclient(inputs)
-
                 elif s == sys.stdin:
                     # handle standard input (ie exit)
                     junk = sys.stdin.readline()
@@ -247,7 +243,6 @@ class ChatServer():
                 else:
                     # handle all other sockets
                     self.handleclients(inputs, s)
-
         self.server.close()
 
 if __name__ == "__main__":
